7479498cb4e90a23d2e62c049817770e4a002e8e
erees <ereese@luc.edu>
2023-03-21 16:52:43 -0500

    Added Underline feature - Emile Reese

--- src/simplejavatexteditor/UI.java
+++ src/simplejavatexteditor/UI.java
@@ -562,7 +562,7 @@ public class UI extends JFrame implements ActionListener {
                 textArea.setFont(textArea.getFont().deriveFont(Font.ITALIC));
             }
         }
-        //for underline - EMILE REESE
+        //for underline - EMILE REESE - Typing to commit.
         else if(e.getSource() == underlineButton){
             HashMap<TextAttribute, Integer> fontAttributes = new HashMap<TextAttribute, Integer>();
             fontAttributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
